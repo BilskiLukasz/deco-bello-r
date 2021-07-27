6b03fee804392b43029d9862c357c64b2ccb3a12
Lukas <lukas@nextgm.ca>
2021-07-27 22:49:41 +0200

    Gallery loading after scroll

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,26 +1,34 @@
 import { imgsGallery } from '../assets/images';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import fakeBG from '../assets/fake-bg.png';
 
 const Gallery = () => {
     const [activeImg, setActiveImg] = useState(imgsGallery[0].url)
+    const [render, setRender] = useState(false)
 
+    useEffect( ()=> {
+        window.addEventListener("scroll", ()=>{setRender(true)});
+    },[])
 
     return ( 
-        <div className="gallery margin-section">                      
-            <div className="gallery-preview" style={{backgroundImage: `url(${activeImg})`}}>
-                <img className="fakeBG" src={fakeBG} alt="empty background"/>
-            </div>
-            <div className="width-container">
-                <p className="gallery-title">Przykładowe prace</p> 
-                <div className="gallery-slider">
-                    {imgsGallery.map(image => 
-                        <div key={image.id} className="gallery-slider-item" onClick={(e) => setActiveImg(image.url)}>
-                            <img key={image.id} className="gallery-slider-img" src={image.url} alt=""/>
-                        </div>
-                    )}
+        <div className="gallery margin-section">
+        {render &&                     
+            <div>
+                <div className="gallery-preview" style={{backgroundImage: `url(${activeImg})`}}>
+                    <img className="fakeBG" src={fakeBG} alt="empty background"/>
                 </div>
-            </div> 
+                <div className="width-container">
+                    <p className="gallery-title">Przykładowe prace</p>
+                    <div className="gallery-slider">
+                        {imgsGallery.map(image =>
+                            <div key={image.id} className="gallery-slider-item" onClick={(e) => setActiveImg(image.url)}>
+                                <img key={image.id} className="gallery-slider-img" src={image.url} alt=""/>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        }
         </div>
      );
 }
